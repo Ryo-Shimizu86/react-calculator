0d34b01cf6708c09654f59bca6d3d70d457bbbc6
Ryo-Shimizu86 <shimizu@1st-link.co.jp>
2022-04-19 06:45:20 +0900

    add button functions

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Button.css";
 
 const isOperator = (val) => {
   return !isNaN(val) || val === "." || val === "=";
 };
 
-const Button = (props) => (
+export const Button = (props) => (
   <div
     className={`button-wrapper ${
       isOperator(props.children) ? null : "operator"
     }`}
+    onClick={() => props.handleClick(props.children)}
   >
     {props.children}
   </div>
 );
-
-export default Button;
